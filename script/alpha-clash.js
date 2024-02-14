019f0a99c9a813ebb4272844a72be42c1aba0a68
mdalihasanriyad <you@ahriyad.com@gmail.com>
2024-02-14 13:04:31 +0600

    added script

---
+++ script/alpha-clash.js
@@ -0,0 +1,15 @@
+// function play() {
+//     // console.log('play now');
+//     // step-1
+//     const homeSection = document.getElementById('home-section');
+//     homeSection.classList.add('hidden');
+        // step-2
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove()
+
+// }
+
+function play(){
+    hideElementById('home-section');
+    showElementById('play-ground');
+}
