393b80a92ff04d6c29651da0ea7f234b98740a53
mdalihasanriyad <you@ahriyad.com@gmail.com>
2024-02-14 15:00:47 +0600

    more added js

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -9,7 +9,20 @@
 
 // }
 
+function continueGame(){
+    const alphabet =getRandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    // show display
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // background color 
+    addBackgroundColorById(alphabet);
+}
+
 function play(){
     hideElementById('home-section');
     showElementById('play-ground');
+    continueGame();
 }
--- script/utility.js
+++ script/utility.js
@@ -7,3 +7,21 @@ function showElementById(elementId){
     const element =document.getElementById(elementId);
     element.classList.remove('hidden');
 }
+
+function addBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+function getRandomAlphabet (){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    // console.log(index, alphabet);
+    return alphabet;
+}
